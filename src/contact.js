3aff78c6dadc4fda08c27f5f30667f383c3bfd3e
Ozioma45 <oziomaegole@gmail.com>
2024-05-21 19:45:40 +0100

    updated the contact page

--- src/contact.js
+++ src/contact.js
@@ -1,14 +1,32 @@
 export default function loadContact() {
   const content = document.getElementById("content");
-  content.innerHTML = ""; // Clear previous content
+  content.innerHTML = "";
 
   const contactDiv = document.createElement("div");
+  contactDiv.classList.add("head2");
   const headline = document.createElement("h1");
   headline.textContent = "Contact Us";
   const description = document.createElement("p");
-  description.textContent = "Reach out to us!";
+  description.textContent =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae";
+
+  const contactList = document.createElement("ul");
+
+  const contactItemTexts = [
+    "08166365570",
+    "oziomaegole@gmail.com",
+    "www.ozibest.com",
+  ];
+
+  contactItemTexts.forEach((itemText) => {
+    const contactItem = document.createElement("li");
+    contactItem.textContent = itemText;
+    contactList.appendChild(contactItem);
+  });
 
   contactDiv.appendChild(headline);
   contactDiv.appendChild(description);
+  contactDiv.appendChild(contactList);
+
   content.appendChild(contactDiv);
 }
